3c1c017d1dfce12ef4a11c0d4ff9d88231143ee1
Ben Wolfe <ben@openmrs.org>
2007-01-09 21:47:19 +0000

    Committing second wave of basicModule --> basicmodule package change

---
+++ src/org/openmrs/module/basicmodule/BasicModuleActivator.java
@@ -0,0 +1,25 @@
+package org.openmrs.module.basicmodule;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.Activator;
+
+public class BasicModuleActivator implements Activator {
+
+	private Log log = LogFactory.getLog(this.getClass());
+
+	/**
+	 * @see org.openmrs.module.Activator#startup()
+	 */
+	public void startup() {
+		log.info("Starting BasicModule");
+	}
+	
+	/**
+	 *  @see org.openmrs.module.Activator#shutdown()
+	 */
+	public void shutdown() {
+		log.info("Shutting down BasicModule");
+	}
+	
+}
