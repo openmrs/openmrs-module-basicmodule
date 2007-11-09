843a80fd6f369303f9f01f264185385000a56808
Ben Wolfe <ben@openmrs.org>
2007-11-09 21:09:19 +0000

    Added a simple test case to basicmodule

--- web/src/org/openmrs/module/basicmodule/web/controller/BasicModuleFormController.java
+++ web/src/org/openmrs/module/basicmodule/web/controller/BasicModuleFormController.java
@@ -1,16 +1,26 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.basicmodule.web.controller;
-
 
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +28,7 @@ import org.springframework.web.servlet.mvc.SimpleFormController;
 import org.springframework.web.servlet.view.RedirectView;
 
 /**
- * This controller backs and saves the printing module printer settings
+ * This controller backs and saves the basic module settings
  * 
  */
 public class BasicModuleFormController extends SimpleFormController {
@@ -27,7 +37,7 @@ public class BasicModuleFormController extends SimpleFormController {
     protected final Log log = LogFactory.getLog(getClass());
         	    
     @Override
-	protected Map referenceData(HttpServletRequest request, Object obj, Errors err) throws Exception {
+	protected Map<String, Object> referenceData(HttpServletRequest request, Object obj, Errors err) throws Exception {
 
 		return new HashMap<String,Object>();
 	}
@@ -35,14 +45,20 @@ public class BasicModuleFormController extends SimpleFormController {
 
 	@Override
 	protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object object, BindException exceptions) throws Exception {		
-    	HttpSession httpSession = request.getSession();
+    	//HttpSession httpSession = request.getSession();
 
-        MessageSourceAccessor msa = getMessageSourceAccessor();
+        //MessageSourceAccessor msa = getMessageSourceAccessor();
 				
     	return new ModelAndView(new RedirectView(getSuccessView()));
     }
 
 
+    /**
+     * This class returns the form backing object.  This can be a string, a boolean, or a normal
+     * java pojo.
+     * 
+     * @see org.springframework.web.servlet.mvc.AbstractFormController#formBackingObject(javax.servlet.http.HttpServletRequest)
+     */
     @Override
 	protected Object formBackingObject(HttpServletRequest request) throws Exception { 
     	return "REPLACE ME";
