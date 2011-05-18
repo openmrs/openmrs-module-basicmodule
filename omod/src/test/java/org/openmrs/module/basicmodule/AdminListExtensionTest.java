7a13a18b67ed16aedf2425a71257ca23d8ed7e69
Ben Wolfe <ben@openmrs.org>
2011-05-18 18:58:00 +0000

    Fixing basicmodule to work out of the box with the maven setup

--- omod/src/test/java/org/openmrs/module/basicmodule/AdminListExtensionTest.java
+++ omod/src/test/java/org/openmrs/module/basicmodule/AdminListExtensionTest.java
@@ -15,36 +15,38 @@ package org.openmrs.module.basicmodule;
 
 import java.util.Map;
 
-import junit.framework.TestCase;
-
+import org.junit.Assert;
+import org.junit.Test;
 import org.openmrs.module.Extension.MEDIA_TYPE;
 import org.openmrs.module.basicmodule.extension.html.AdminList;
 
 /**
  * This test validates the AdminList extension class
  */
-public class AdminListExtensionTest extends TestCase {
-
+public class AdminListExtensionTest {
+	
 	/**
 	 * Get the links for the extension class
 	 */
+	@Test
 	public void testValidatesLinks() {
 		AdminList ext = new AdminList();
 		
 		Map<String, String> links = ext.getLinks();
 		
-		assertNotNull("Some links should be returned", links);
+		Assert.assertNotNull("Some links should be returned", links);
 		
-		assertTrue("There should be a positive number of links", links.values().size() > 0);
+		Assert.assertTrue("There should be a positive number of links", links.values().size() > 0);
 	}
 	
 	/**
 	 * Check the media type of this extension class
 	 */
+	@Test
 	public void testMediaTypeIsHtml() {
 		AdminList ext = new AdminList();
 		
-		assertTrue("The media type of this extension should be html", ext.getMediaType().equals(MEDIA_TYPE.html));
+		Assert.assertTrue("The media type of this extension should be html", ext.getMediaType().equals(MEDIA_TYPE.html));
 	}
 	
 }
