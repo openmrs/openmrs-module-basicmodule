71bd75de6a64cc0e692d9da797a8cbcf7eb3b324
tmvumbi <tresormvumbi@gmail.com>
2018-09-19 08:29:20 +0200

    Fix module activator (#4)
    * Fix module activator
    
    The class BasicModuleActivator was implementing Activator (currently deprecated); this resulted to the module failing to start. This commit fixes the issue by extending BaseModuleActivator.
    
    * Remove unused dependency
    
    The dependency org.openmrs.api is not needed by the basic module

--- api/src/main/java/org/openmrs/module/basicmodule/BasicModuleActivator.java
+++ api/src/main/java/org/openmrs/module/basicmodule/BasicModuleActivator.java
@@ -15,12 +15,12 @@ package org.openmrs.module.basicmodule;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.module.Activator;
+import org.openmrs.module.BaseModuleActivator;
 
 /**
  * This class contains the logic that is run every time this module is either started or shutdown
  */
-public class BasicModuleActivator implements Activator {
+public class BasicModuleActivator extends BaseModuleActivator {
 	
 	private Log log = LogFactory.getLog(this.getClass());
 	
