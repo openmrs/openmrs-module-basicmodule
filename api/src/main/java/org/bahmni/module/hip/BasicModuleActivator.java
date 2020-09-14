2470139ac500845d57c05000c1a249c23e4ad535
sonawaneabhijeet <abhijeet.sonawane@thoughtworks.com>
2020-09-14 12:55:48 +0530

    HIP-62 | Abhijeet/David | Adds changes suggested by Bindu

--- api/src/main/java/org/bahmni/module/hip/BasicModuleActivator.java
+++ api/src/main/java/org/bahmni/module/hip/BasicModuleActivator.java
@@ -16,26 +16,27 @@ package org.bahmni.module.hip;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.module.ModuleActivator;
 
 /**
  * This class contains the logic that is run every time this module is either started or shutdown
  */
 public class BasicModuleActivator extends BaseModuleActivator {
-	
+
 	private Log log = LogFactory.getLog(this.getClass());
-	
+
 	/**
 	 * @see org.openmrs.module.Activator#startup()
 	 */
 	public void startup() {
 		log.info("Starting Basic Module");
 	}
-	
+
 	/**
 	 * @see org.openmrs.module.Activator#shutdown()
 	 */
 	public void shutdown() {
 		log.info("Shutting down Basic Module");
 	}
-	
+
 }
