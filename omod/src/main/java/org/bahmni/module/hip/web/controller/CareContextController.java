dcbe30b71d56cdb70a10d8174cdacdb6d952c7b3
avghadsangita <sangita.avghad@thoughtworks.com>
2020-10-01 16:11:51 +0530

    HIP-89 | Raghavi/Sangita | Adds api for fetching patient care context

---
+++ omod/src/main/java/org/bahmni/module/hip/web/controller/CareContextController.java
@@ -0,0 +1,31 @@
+package org.bahmni.module.hip.web.controller;
+
+import org.bahmni.module.hip.model.PatientCareContext;
+import org.bahmni.module.hip.web.service.CareContextService;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/hip")
+@RestController
+public class CareContextController {
+    private final CareContextService careContextService;
+    @Autowired
+    public CareContextController(CareContextService careContextService) {
+        this.careContextService = careContextService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/careContext", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<PatientCareContext>> getCareContextForPatient(@RequestParam String patientId){
+        return ResponseEntity.ok(careContextService.careContextForPatient(patientId));
+    }
+}
