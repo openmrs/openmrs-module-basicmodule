46116bd674149c3f1fbc42e03139d18ebff7a9bd
SanoferSameera <sanofersameera25@gmail.com>
2022-08-18 13:11:33 +0530

    BAH-2167 | Refactor. to send visit dateTime in careContext

--- omod/src/main/java/org/bahmni/module/hip/web/model/FhirDiagnosticReport.java
+++ omod/src/main/java/org/bahmni/module/hip/web/model/FhirDiagnosticReport.java
@@ -24,7 +24,7 @@ import static org.bahmni.module.hip.web.service.Constants.DOCUMENT_TYPE;
 
 public class FhirDiagnosticReport {
     private final List<DiagnosticReport> diagnosticReports;
-    private final Date encounterTimestamp;
+    private final Date visitTimestamp;
     private final Integer encounterID;
     private final Encounter encounter;
     private final List<Practitioner> practitioners;
@@ -32,7 +32,7 @@ public class FhirDiagnosticReport {
     private final Reference patientReference;
     private final List<Observation> obs;
 
-    private FhirDiagnosticReport(Date encounterDatetime,
+    private FhirDiagnosticReport(Date visitDateTime,
                                  List<DiagnosticReport> diagnosticReports,
                                  Integer encounterID,
                                  Encounter encounter,
@@ -40,7 +40,7 @@ public class FhirDiagnosticReport {
                                  Patient patient,
                                  Reference patientReference,
                                  List<Observation> obs) {
-        this.encounterTimestamp = encounterDatetime;
+        this.visitTimestamp = visitDateTime;
         this.diagnosticReports = diagnosticReports;
         this.encounterID = encounterID;
         this.encounter = encounter;
@@ -53,7 +53,7 @@ public class FhirDiagnosticReport {
 
     public Bundle bundleDiagnosticReport(String webUrl) {
         String bundleID = String.format("PR-%d", encounterID);
-        Bundle bundle = FHIRUtils.createBundle(encounterTimestamp, bundleID, webUrl);
+        Bundle bundle = FHIRUtils.createBundle(visitTimestamp, bundleID, webUrl);
 
         FHIRUtils.addToBundleEntry(bundle, compositionFrom(webUrl), false);
         FHIRUtils.addToBundleEntry(bundle, practitioners, false);
@@ -70,7 +70,7 @@ public class FhirDiagnosticReport {
         Patient patient = fhirResourceMapper.mapToPatient(openMrsDiagnosticReport.getPatient());
         Reference patientReference = FHIRUtils.getReferenceToResource(patient);
         Encounter encounter = fhirResourceMapper.mapToEncounter(openMrsDiagnosticReport.getEncounter());
-        Date encounterDatetime = openMrsDiagnosticReport.getEncounter().getEncounterDatetime();
+        Date visitDateTime = openMrsDiagnosticReport.getEncounter().getVisit().getDateCreated();
         Integer encounterId = openMrsDiagnosticReport.getEncounter().getId();
         List<Practitioner> practitioners = getPractitionersFrom(fhirResourceMapper, openMrsDiagnosticReport.getEncounterProviders());
         List<Obs> observationNotDocumentType = openMrsDiagnosticReport.getEncounter().getAllObs().stream()
@@ -87,12 +87,12 @@ public class FhirDiagnosticReport {
                     diagnosticReport.setResult(observations.stream().map(FHIRUtils::getReferenceToResource)
                             .collect(Collectors.toList()));
                 }).collect(Collectors.toList());
-        return new FhirDiagnosticReport(encounterDatetime, diagnosticReportList, encounterId, encounter, practitioners,
+        return new FhirDiagnosticReport(visitDateTime, diagnosticReportList, encounterId, encounter, practitioners,
                 patient, patientReference, observations);
     }
 
     private Composition compositionFrom(String webURL) {
-        Composition composition = initializeComposition(encounterTimestamp, webURL);
+        Composition composition = initializeComposition(visitTimestamp, webURL);
         Composition.SectionComponent compositionSection = composition.addSection();
 
         practitioners
